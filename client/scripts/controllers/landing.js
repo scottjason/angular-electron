c70f87708b27d1bf99d40d502183387a79bd6fd3
Scott Jason <scottleejason@gmail.com>
2015-10-12 23:37:01 -0700

    renders new customer, fitting room alert and item selected in electron desktop app

--- client/scripts/controllers/landing.js
+++ client/scripts/controllers/landing.js
@@ -5,29 +5,75 @@ angular.module('app')
 
 function LandingCtrl($scope, $rootScope, $state, $timeout, StateService) {
 
-  var ipc = require('ipc');
-
-  ipc.send('asynchronous-message', 'ping');
-
   var Firebase = require('firebase');
   var fittingRoom = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/products');
   var customerStore = new Firebase('https://retail-store-app.firebaseio.com/customers');
+  var likedItems = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/liked-items');
+  var dislikedItems = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/disliked-items');
 
-  $scope.customers = [];
+  $scope.customer = {};
+  $scope.timer = '00:00:00';
 
   function init() {
-    fittingRoom.on("value", function(snapshot) {
-        $scope.products = [];
-        var products = snapshot.val();
-        console.log('fittingRoom', products);
+   
+    customerStore.on("child_added", function(snapshot, prevChildKey) {
+        var customer = snapshot.val();
+        if (customer) {
+          $timeout(function() {
+            console.log(customer)
+            $scope.customer.name = customer.name;
+          });
+        }
+        console.log('customer added', $scope.customer);
       },
       function(errorObject) {
         console.log("The read failed: " + errorObject.code);
       });
 
-    customerStore.on("value", function(snapshot) {
-        var customers = snapshot.val();
-        console.log('customer modified', customers)
+    customerStore.on("child_removed", function(snapshot, prevChildKey) {
+        $timeout(function() {
+          $scope.customer.name = '';
+        });
+        console.log('customer removed', $scope.customer);
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+
+    fittingRoom.on("child_added", function(snapshot) {
+        $scope.product = {};
+        var product = snapshot.val();
+        $scope.product.title = product.title;
+        $scope.product.cost = product.cost;
+        $scope.product.photo = product.photo;
+        console.log('fittingRoom item added', product);
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+
+    fittingRoom.on("child_removed", function(snapshot) {
+        $scope.product.title = "";
+        $scope.product.cost = "";
+        $scope.product.photo = "";
+        console.log('fittingRoom item removed', product);
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+
+
+    likedItems.on("value", function(snapshot) {
+        var likedItems = snapshot.val();
+        console.log('liked item modified', likedItems)
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+
+    dislikedItems.on("value", function(snapshot) {
+        var dislikedItems = snapshot.val();
+        console.log('disliked item modified', dislikedItems)
       },
       function(errorObject) {
         console.log("The read failed: " + errorObject.code);
@@ -35,15 +81,6 @@ function LandingCtrl($scope, $rootScope, $state, $timeout, StateService) {
   }
 
   init();
-
-  $scope.getRequests = function() {
-    return StateService.data['Request'];
-  }
-
-  $scope.getState = function(key) {
-    return StateService.data['Request'][key];
-  };
-
 
   LandingCtrl.$inject['$scope', '$rootScope', '$state', '$timeout', 'StateService'];
 }
